12e78302bb73253a5f30b656eedef701be51626e
u-masato <masato.u20@gmai.com>
2021-08-22 10:33:32 +0000

    finish ex1

--- src/App.js
+++ src/App.js
@@ -106,6 +106,11 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+			<ol>
+            {profiles.map((profile) => (
+              <li key={profile.id}>{users[profile.userID].name}'s favorite movie is  { movies[profile.favoriteMovieID].name }</li>
+            ))}
+			</ol>
       </div>
     );
   }
